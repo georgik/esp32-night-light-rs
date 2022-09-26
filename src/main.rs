d082afda818b808f9548c015b3535df252902188
Juraj Michálek <juraj.michalek@espressif.com>
2022-09-26 15:59:01 +0200

    got infrared running in main loop

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,16 @@
 #![no_main]
 
 extern crate alloc;
-use esp32c3_hal::{clock::ClockControl, pac::Peripherals, prelude::*, timer::TimerGroup, Rtc,
+
+use esp32c3_hal::{
+    clock::ClockControl,
+    interrupt,
+    pac::{self, Peripherals, TIMG0, TIMG1},
+    prelude::*,
+    timer::{Timer, Timer0, TimerGroup},
+    Rtc,
+};
+use esp32c3_hal::{ prelude::*,
     gpio::{Gpio5, IO},
     gpio_types::{Event, Input, Pin, PullDown, PullUp, Floating},
     pulse_control::ClockSource,
@@ -11,15 +20,18 @@ use esp32c3_hal::{clock::ClockControl, pac::Peripherals, prelude::*, timer::Time
     utils::{smartLedAdapter, SmartLedsAdapter},
 };
 use esp_backtrace as _;
+use critical_section::Mutex;
+use core::cell::RefCell;
 #[global_allocator]
 static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
-
+static TIMER0: Mutex<RefCell<Option<Timer<Timer0<TIMG0>>>>> = Mutex::new(RefCell::new(None));
 use infrared::{
     protocol::{AppleNec, Nec, NecDebug},
     remotecontrol::{nec::*, rc5::*},
     remotecontrol::{Action, Button},
     Receiver,
     cmd::AddressCommand
+
 };
 
 use smart_leds::{
@@ -46,19 +58,21 @@ fn init_heap() {
 
 
 type IrPin = Gpio5<Input<Floating>>;
-type IrReceiver = infrared::PeriodicPoll<NecDebug, IrPin>;
-const SAMPLERATE: u32 = 1_000_000;
+type IrReceiver = infrared::PeriodicPoll<Nec, IrPin>;
+static mut RECEIVER: Option<IrReceiver> = None;
 
 #[riscv_rt::entry]
 fn main() -> ! {
     let peripherals = Peripherals::take().unwrap();
     let mut system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+    // let clocks = ClockControl::configure(system.clock_control, c)  .freeze();
 
     // Disable the RTC and TIMG watchdog timers
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
     let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
     let mut wdt0 = timer_group0.wdt;
+    let mut timer0 = timer_group0.timer0;
     let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
     let mut wdt1 = timer_group1.wdt;
 
@@ -66,6 +80,11 @@ fn main() -> ! {
     rtc.rwdt.disable();
     wdt0.disable();
     wdt1.disable();
+
+
+    const FREQ: u32 = 38_000;
+    interrupt::enable(pac::Interrupt::TG0_T0_LEVEL, interrupt::Priority::Priority1).unwrap();
+
 
     // Configure RMT peripheral globally
     let pulse = PulseControl::new(
@@ -98,34 +117,83 @@ fn main() -> ! {
     let mut irda_pin = io.pins.gpio5.into_floating_input();
     // let mut receiver:IrReceiver = infrared::PeriodicPoll::with_pin(SAMPLERATE, irda_pin);
 
- use infrared::{Receiver,
-     remotecontrol::rc5::CdPlayer, cmd::AddressCommand,
-    
-     protocol::rc5::Rc5Command,
- };
+    use infrared::{Receiver,
+        remotecontrol::rc5::CdPlayer, cmd::AddressCommand,
+        protocol::rc5::Rc5Command,
+    };
 
-    const RESOLUTION: u32 = 1_000_000;
-    let mut receiver = Receiver::builder()
-         .nec()
-         .frequency(RESOLUTION)
-         .pin(irda_pin)
-         .remotecontrol(Apple2009)
-         .build();
+    // const RESOLUTION: u32 = 1_000_000;
+    // let mut receiver = Receiver::builder()
+    //      .nec()
+    //      .frequency(RESOLUTION)
+    //      .pin(irda_pin)
+    //      .build();
+
+
+    use infrared::{
+        // Receiver,
+        receiver::{NoPin, Builder},
+        protocol::{Rc6, Nec},
+    };
+    // use dummy_pin::DummyPin;
+    use infrared::receiver::BufferInputReceiver;
+    // Frequency of the timer interrupt in Hz.
+    let mut receiver = infrared::PeriodicPoll::<Nec, Gpio5<Input<Floating>>>::with_pin(FREQ, irda_pin);
+    // unsafe {
+    //     RECEIVER.replace(receiver);
+    //     }
+
+    // timer0.start(25u64.micros());
+    // timer0.listen();
+
+    // critical_section::with(|cs| {
+    //     TIMER0.borrow_ref_mut(cs).replace(timer0);
+    // });
+
+    // unsafe {
+    //     riscv::interrupt::enable();
+    // }
+
+
+
+    // // Receiver for Rc6 signals, event based with embedded-hal pin
+    // let pin = irda_pin;
+    // let r1: Receiver<Rc6, Gpio5<Input<PullDown>>> = Receiver::with_pin(40_000, pin);
+
+    // Periodic polled Nec Receiver
+    // let pin = DummyPin::new_low();
+    // let r2: infrared::PeriodicPoll<Nec, DummyPin> = infrared::PeriodicPoll::with_pin(40_000, pin);
+
+    // let mut r3: BufferInputReceiver<NecDebug> = BufferInputReceiver::with_frequenzy(20_000);
+
+    // let buf: &[u32] = &[20, 40, 20];
+    // let cmd_iter = r3.iter(buf);
 
     println!("Starting main loop");
     loop {
-        for hue in 0..=255 {
+        // for hue in 0..=255 {
+            // let receiver = unsafe { RECEIVER.as_mut().unwrap() };
 
-            let dt = 0; // Time since last pin flip
-
-            if let Ok(Some(button)) = receiver.event(dt) {
-                // Get the command associated with this button
-                let cmd = button.command();
-                println!(
-                    "Action: {:?} - (Address, Command) = ({}, {})",
-                    button.action(), cmd.address(), cmd.command()
-                );
+            if let Ok(Some(cmd)) = receiver.poll() {
+                println!("A:{} C:{}", cmd.addr, cmd.cmd);
             }
+            // if let Ok(Some(cmd)) = receiver.poll() {
+            //     println!("x{}", cmd.addr);
+            // }
+            delay.delay_us(26u32);
+
+            // let dt = 0; // Time since last pin flip
+
+            // if let Ok(Some(button)) = receiver.event(dt) {
+            //     // Get the command associated with this button
+            //     let cmd = button.cmd;
+            //     println!(
+            //         "Action: {:?} - (Address, Command) = ()",
+            //         // "Action: {:?} - (Address, Command) = ({}, {})",
+            //         cmd
+            //         // button.addr .action(), cmd.address(), cmd.command()
+            //     );
+            // }
                         // color.hue = hue;
             // // Convert from the HSV color space (where we can easily transition from one
             // // color to the other) to the RGB color space that we can then send to the LED
@@ -154,6 +222,24 @@ fn main() -> ! {
             //     Ok(None) => { }
             //     Err(err) => println!("Err: {:?}", err),
             // }
-        }
+        // }
     }
+}
+
+
+#[interrupt]
+fn TG0_T0_LEVEL() {
+    critical_section::with(|cs| {
+        // esp_println::println!("Interrupt 1");
+        let receiver = unsafe { RECEIVER.as_mut().unwrap() };
+
+        if let Ok(Some(cmd)) = receiver.poll() {
+            println!("x{}", cmd.addr);
+        }
+        let mut timer0 = TIMER0.borrow_ref_mut(cs);
+        let timer0 = timer0.as_mut().unwrap();
+
+        timer0.clear_interrupt();
+        timer0.start(25u64.micros());
+    });
 }
